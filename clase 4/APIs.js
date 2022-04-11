98edb6c5083b19170ffa9e897cd42160f8a1472c
Pablo Litvin <pablo.litvin@gmail.com>
2022-04-11 19:44:42 -0300

    con paginator

--- clase 4/APIs.js
+++ clase 4/APIs.js
@@ -1,11 +1,25 @@
 const start = async () => {
     try{    
         const {info, results} = await getCharactersByStringFiltro('?');   
-        showPersonajes(results);
+        showPersonajes(results);        
+        setPagination(info);
     }
     catch(err){
         console.error(err);
     }
+}
+function setPagination(info){
+    console.log("Paginador previo:",info.prev);
+    console.log("Paginador next:",info.next);
+    document.getElementById("paginator").innerHTML="";
+    if(info.prev!==null){ //si hay un previo agrego boton
+        const previousButton=`<li class="page-item"><a class="page-link" href="javascript:search('${info.prev}');">Previous</a></li>`;
+        document.getElementById("paginator").innerHTML= previousButton;
+    }
+    if(info.next!==null){
+        const nextButton=`<li class="page-item"><a class="page-link" href="javascript:search('${info.next}');">Next</a></li>`;        
+        document.getElementById("paginator").innerHTML+= nextButton;
+    }    
 }
 function showPersonajes(characters){
     document.getElementById("cards").innerHTML = '';
@@ -30,7 +44,7 @@ function showPersonajes(characters){
         document.getElementById("cards").insertAdjacentHTML('beforeend', '<h5 class="text-white">sin resultados</h5>');
     }    
 }
-const getFilter = () =>{
+const getFilter = () =>{    
     let filtros=new Array();
     let name =document.getElementById('name').value;
     let status =document.getElementById('status').value;
@@ -46,19 +60,20 @@ const getFilter = () =>{
     } 
     return filtros;
 }
-const search = async () => {
+const search = async (page) => {       
     let filtros=getFilter();
     let stringFiltro="?";
     for (let atributo in filtros){
         stringFiltro+='&'+atributo+'='+filtros[atributo];
-    }
-    const {info, results} = await getCharactersByStringFiltro(stringFiltro);    
-    showPersonajes(results); 
+    }      
+    const {info, results} = await getCharactersByStringFiltro(stringFiltro,page);    
+    showPersonajes(results);     
+    setPagination(info);// con cada busqueda actualizo la paginacion segun lo que devuelve info.
     
 }
-const getCharactersByStringFiltro = async (stringFiltro) =>{
-    console.log(stringFiltro);
-    const response = await fetch('https://rickandmortyapi.com/api/character'+stringFiltro);
+const getCharactersByStringFiltro = async (stringFiltro,page) =>{
+    page=page || 'https://rickandmortyapi.com/api/character'+stringFiltro; //si no pasan una pagina especifica, muestro la primera+sus filtros
+    const response = await fetch(page);
     const {info, results} = await response.json(); 
     return {info, results};    
 }
